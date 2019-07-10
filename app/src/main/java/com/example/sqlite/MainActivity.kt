993445dba33a0489e0acf4a4d515cb2ca7bd927b
unknown <marco.balderaspq@gmail.com>
2019-07-10 13:18:26 -0500

    Funciones a los botones y conexion a la base de datos

--- app/src/main/java/com/example/sqlite/MainActivity.kt
+++ app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -2,11 +2,133 @@ package com.example.sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var no:String =""
+    var nom:String=""
+    var edad:String="0"
+    var carrera:String=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buscarEstudiante (v:View){
+        if(etcontrol.text.isEmpty()){
+            Toast.makeText(this,"Campos vacios",Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            no = etcontrol.text.toString()
+            val admin = adBD(this)
+                                                        //0      //1     //2       //3
+            val tupla= admin.consulta("SELECT noControl, nomEst, carrera, edadEst FROM Estudiante WHERE noControl='$no'")
+
+            if (tupla!!.moveToFirst())
+            {
+                etnombre.setText(tupla.getString(1))
+                etcarrera.setText(tupla.getString(2))
+                etedad.setText(tupla.getString(3))
+                btnagregar.isEnabled=false
+                btneliminar.isEnabled=true
+                btnmodificar.isEnabled=true
+            }
+            else
+                {
+                    Toast.makeText(this, "No existe el nuemero de control", Toast.LENGTH_SHORT).show();
+                    etcontrol.requestFocus()
+                }
+        }
+    }
+    fun agregaEstudiante(v: View){
+        if (etcontrol.text.isEmpty() || etnombre.text.isEmpty()||etcarrera.text.isEmpty()||etedad.text.isEmpty()){
+            Toast.makeText(this, "Campos vacios", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            leercajas()
+
+            val sentecia :String="INSERT INTO Estudiante(noControl, nomEst, carrera, edadEst)VALUES('$no','$nom','$carrera',$edad)"
+            val admin = adBD(this)
+
+            if(admin.Ejecuta(sentecia) ==1){
+                Toast.makeText(this, "Estudiante agregado", Toast.LENGTH_SHORT).show();
+                limpiarcaja()
+            }else
+            {
+                Toast.makeText(this, "Estudiante no agregado", Toast.LENGTH_SHORT).show();
+                etcontrol.requestFocus()
+            }
+        }
+    }
+
+    fun modificarEstudiante(v:View){
+        if (etcontrol.text.isEmpty() || etnombre.text.isEmpty()||etcarrera.text.isEmpty()||etedad.text.isEmpty()){
+            Toast.makeText(this, "Campos vacios", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            leercajas()
+
+            val sentecia :String="UPDATE Estudiante SET nomEst ='$nom', carrera='$carrera', edadEst=$edad WHERE noControl='$no'"
+            val admin = adBD(this)
+
+            if(admin.Ejecuta(sentecia) ==1){
+                Toast.makeText(this, "Estudiante modificado", Toast.LENGTH_SHORT).show();
+                limpiarcaja()
+            }else
+            {
+                Toast.makeText(this, "Estudiante no modificado", Toast.LENGTH_SHORT).show();
+                etcontrol.requestFocus()
+            }
+        }
+
+    }
+
+    fun elimaEstudiante(v:View){
+        if (etcontrol.text.isEmpty()){
+            Toast.makeText(this, "Insertar numero de control", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            leercajas()
+            val sentecia :String="DELETE FROM Estudiante where noControl='$no'"
+            val admin = adBD(this)
+
+            if(admin.Ejecuta(sentecia) ==1){
+                Toast.makeText(this, "Estudiante eliminado", Toast.LENGTH_SHORT).show();
+                limpiarcaja()
+            }else
+            {
+                Toast.makeText(this, "Estudiante no eliminado", Toast.LENGTH_SHORT).show();
+                etcontrol.requestFocus()
+            }
+
+        }
+    }
+
+    fun leercajas(){
+        no=etcontrol.text.toString()
+        nom=etnombre.text.toString()
+        carrera=etcarrera.text.toString()
+        edad=etedad.text.toString()
+    }
+    fun limpiarcaja(){
+        no=""
+        nom=""
+        carrera=""
+        edad="0"
+        etcontrol.setText("")
+        etnombre.setText("")
+        etcarrera.setText("")
+        etedad.setText("0")
+        btnagregar.isEnabled=true
+        btnmodificar.isEnabled=false
+        btneliminar.isEnabled=false
     }
 }
--- app/src/main/java/com/example/sqlite/adBD.kt
+++ app/src/main/java/com/example/sqlite/adBD.kt
@@ -20,34 +20,37 @@ class adBD (context: Context) :SQLiteOpenHelper(context, DATABASE, null, 1)
                         "edadEst Integer)"
         )
     }
-    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
-        fun Ejecuta(sentencia:String):Int {
-            try
-            {
-                val db=this.writableDatabase// se abre base datos en base modo lectura
-                db.execSQL(sentencia)
-                db.close()
-                return  1
-            }
-            catch (ex: Exception)
-            {
-             return 0
-            }
-        }
 
-        fun consulta(select: String):Cursor? {
-            try
-            {
-                val db = this.readableDatabase
-                return db.rawQuery(select,null)
-            }
-            catch (ex:Exception)
-            {
-                return null
-            }
-        }
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
 
     }
+
+    fun consulta(select: String):Cursor? {
+        try
+        {
+            val db = this.readableDatabase
+            return db.rawQuery(select,null)
+        }
+        catch (ex:Exception)
+        {
+            return null
+        }
+    }
+
+    fun Ejecuta(sentencia:String):Int {
+        try
+        {
+            val db=this.writableDatabase// se abre base datos en base modo lectura
+            db.execSQL(sentencia)
+            db.close()
+            return  1
+        }
+        catch (ex: Exception)
+        {
+            return 0
+        }
+    }
+
 
 
 }
